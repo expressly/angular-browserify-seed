ac8726bcd239aaed2c49ecb19316c3ff90fe318f
Anton Myshenin <myshenin.contact@gmail.com>
2016-12-03 13:25:03 +0200

    prepare

--- src/modules/app/foo/index.js
+++ src/modules/app/foo/index.js
@@ -4,4 +4,5 @@ module.exports =
     angular.module('expressly.foo', [])
         .config(require('./routes.js'))
         .config(require('./i18n/en.js'))
-        .controller('barController', require('./controllers/bar.controller'));
+        .controller('barController', require('./controllers/bar.controller'))
+        .controller('imageViewController', require('./controllers/imageViewController'));
