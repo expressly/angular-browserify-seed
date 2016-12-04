caec6e4a8ba7469d762b32244d5ee6cf2436a1c6
Anton Myshenin <myshenin.contact@gmail.com>
2016-12-04 12:41:15 +0200

    main page

--- src/modules/app/foo/index.js
+++ src/modules/app/foo/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports =
-    angular.module('expressly.foo', [])
-        .config(require('./routes.js'))
-        .config(require('./i18n/en.js'))
-        .controller('barController', require('./controllers/bar.controller'))
-        .controller('imageViewController', require('./controllers/imageViewController'));
+  angular.module('expressly.foo', [])
+    .config(require('./routes.js'))
+    .config(require('./i18n/en.js'))
+    .controller('barController', require('./controllers/bar.controller'))
+    .controller('imageViewController', require('./controllers/imageViewController'))
+    .controller('mainController', require('./controllers/mainController'));
--- src/modules/app/foo/routes.js
+++ src/modules/app/foo/routes.js
@@ -3,11 +3,15 @@
 module.exports = /*ngInject*/
   function ($routeProvider) {
     $routeProvider
+      .when("/", {
+        templateUrl: "app/foo/templates/main.html",
+        controller: "mainController"
+      })
       .when("/image/:id", {
         templateUrl: "app/foo/templates/image-view.html",
         controller: "imageViewController"
       })
       .otherwise({
-        redirectTo: '/image/1'
+        redirectTo: '/'
       });
   };
